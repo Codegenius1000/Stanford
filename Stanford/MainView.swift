004ec48b817d20c01f095fa5ad6dba7ae9ad4392
Codegenius1000 <kztonyliu@gmail.com>
2024-07-23 18:45:04 -0400

    Change name to mainview

---
+++ Stanford/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stanford
+//
+//  Created by Tony Liu on 7/23/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, this is a test!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ Stanford/StanfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordApp.swift
+//  Stanford
+//
+//  Created by Tony Liu on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct StanfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
